7f24059bc0329ea82742ec4b57f1ba6da2a30c8c
Pat Nakajima <patnakajima@gmail.com>
2025-06-23 23:02:38 -0700

    cleanup a bit

--- src/prelude.rs
+++ src/prelude.rs
@@ -22,24 +22,6 @@ pub fn compile_prelude() -> &'static Prelude {
     &PRELUDE_TYPED
 }
 
-// pub fn _compile_prelude_for_name_resolver() -> Prelude {
-//     let source = &[
-//         load_stdlib_module("Optional").unwrap(),
-//         load_stdlib_module("Array").unwrap(),
-//     ]
-//     .join("\n");
-//     let mut symbol_table = SymbolTable::base();
-//     let parsed = parse(source, "prelude".into());
-//     let _ = NameResolver::new(&mut symbol_table).resolve(parsed, &mut symbol_table);
-
-//     Prelude {
-//         symbols: symbol_table,
-//         types: Default::default(),
-//         schemes: Default::default(),
-//         typed_exprs: Default::default(),
-//     }
-// }
-
 pub fn _compile_prelude() -> Prelude {
     let mut driver = Driver::new(DriverConfig {
         executable: false,
