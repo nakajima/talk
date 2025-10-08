75130ece33694bb2976ed6f7da86c9c67f2fbd84
Pat Nakajima <patnakajima@gmail.com>
2025-10-08 10:55:42 -0700

    wip

--- src/ir/lowerer.rs
+++ src/ir/lowerer.rs
@@ -1,12 +1,15 @@
 use crate::{ast::AST, name_resolution::name_resolver::NameResolved, types::type_session::Types};
 
 pub struct Lowerer<'a> {
-    ast: &'a AST<NameResolved>,
-    types: &'a Types,
+    _ast: &'a AST<NameResolved>,
+    _types: &'a Types,
 }
 
 impl<'a> Lowerer<'a> {
     pub fn new(ast: &'a AST<NameResolved>, types: &'a Types) -> Self {
-        Self { ast, types }
+        Self {
+            _ast: ast,
+            _types: types,
+        }
     }
 }
