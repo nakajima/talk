9c5fcca59960952c8a788a0d2968a70bfc8d461d
Pat Nakajima <patnakajima@gmail.com>
2025-11-21 12:42:32 -0800

    remove dead code

--- talk-ex/gen.sh
+++
@@ -1,9 +0,0 @@
-
-#! /usr/bin/bash
-
-echo "Generating component"
-cargo component build --target wasm32-wasip1 --release --artifact-dir dist/wit -Z unstable-options --verbose
-wasm-tools component unbundle target/wasm32-wasip1/release/talk_ex.wasm --module-dir dist/unbundled -o dist/unbundled/talk_ex_core.wasm
-
-mkdir -p ../../talk.swift/Sources/TalkTalk/wasm
-cp dist/unbundled/* ../../talk.swift/Sources/TalkTalk/wasm/
