06ee6dd7d923e00116ad42010125cdd8e1819d29
Pat Nakajima <patnakajima@gmail.com>
2026-01-20 00:44:56 -0800

    dead code

--- src/name_resolution/transforms/prepend_self_to_methods.rs
+++ src/name_resolution/transforms/prepend_self_to_methods.rs
@@ -6,7 +6,6 @@ use crate::{
     node_id::{FileID, NodeID},
     node_kinds::{
         decl::{Decl, DeclKind},
-        expr::Expr,
         parameter::Parameter,
         type_annotation::{TypeAnnotation, TypeAnnotationKind},
     },
@@ -14,7 +13,7 @@ use crate::{
 };
 
 #[derive(VisitorMut)]
-#[visitor(Decl(enter), Expr(enter))]
+#[visitor(Decl(enter))]
 pub struct PrependSelfToMethods {
     file_id: FileID,
     node_ids: IDGenerator,
@@ -31,42 +30,6 @@ impl PrependSelfToMethods {
             root.drive_mut(&mut pass);
         }
         _ = std::mem::replace(&mut ast.node_ids, pass.node_ids);
-    }
-
-    fn enter_expr(&mut self, _expr: &mut Expr) {
-        // if let ExprKind::Call {
-        //     callee:
-        //         box Expr {
-        //             kind: ExprKind::Member(Some(receiver), ..),
-        //             ..
-        //         },
-        //     args,
-        //     ..
-        // } = &mut expr.kind
-        //     && let box Expr {
-        //         kind: ExprKind::Variable(name),
-        //         ..
-        //     } = receiver
-        //     && *name == Name::Raw("self".to_string())
-        // {
-        //     args.insert(
-        //         0,
-        //         CallArg {
-        //             id: receiver.id,
-        //             value: *receiver.clone(),
-        //             label: Label::Positional(0),
-        //             label_span: Span::SYNTHESIZED,
-        //             span: receiver.span,
-        //         },
-        //     );
-
-        //     *receiver = Expr {
-        //         id: NodeID(self.file_id, self.node_ids.next_id()),
-        //         span: Span::SYNTHESIZED,
-        //         kind: ExprKind::Variable(Name::Raw("Self".into())),
-        //     }
-        //     .into();
-        // }
     }
 
     fn enter_decl(&mut self, decl: &mut Decl) {
